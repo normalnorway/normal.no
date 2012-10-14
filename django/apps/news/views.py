4b704274f1b080e7004516c82f30eadfa0d6b20f
Torkel Bjørnson-Langen <torkel.bjornson@gmail.com>
2012-10-14 13:47:59 +0200

    news: added paginator

--- django/apps/news/views.py
+++ django/apps/news/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render, render_to_response, get_object_or_404
-from apps.news.models import Article
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from .models import Article
 
 
 def list (request):
-    articles = Article.objects.all().order_by('-date')[:25]
-    return render (request, 'news/list.html', { 'list': articles })
+    #articles = Article.objects.all().order_by('-date')[:25]
+    pagesize = 25
+    paginator = Paginator (Article.objects.all().order_by('-date'), pagesize)
+    try:
+        articles = paginator.page (request.GET.get('page'))
+    except:
+        articles = paginator.page (1)
+
+    # @todo helper?
+    low = (articles.number-1) * pagesize + 1
+    high = low + pagesize
+    count = articles.paginator.count
+    if high > count: high = count
+
+    return render (request, 'news/list.html', {
+        'list': articles, 'low': low, 'high': high,
+    })
 
 
 def detail (request, news_id):
-    item = get_object_or_404 (Article, pk=news_id)
-    return render (request, 'news/detail.html', { 'item': item })
+    return render (request, 'news/detail.html', {
+        'item': get_object_or_404 (Article, pk=news_id)
+        # @todo same name as DetailView uses. (object?)
+    })
