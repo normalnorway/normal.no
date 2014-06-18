312a2999960a6ae5f131f204dbf6b6b5402cacd0
Torkel Bjørnson-Langen <torkel@normal.no>
2014-06-18 11:56:54 +0200

    minor

--- django/apps/support/views.py
+++ django/apps/support/views.py
@@ -1,5 +1,4 @@
 # encoding: utf-8
-
 import os
 import json
 import datetime
@@ -22,8 +21,8 @@ en giro per e-post eller brev.
 @render_to ('support:index.html')
 def index (request):
     if not request.method == 'POST':
-        return dict (form=MemberForm())
-    form = MemberForm (request.POST)
+        return dict (form=MemberForm(label_suffix=''))
+    form = MemberForm (request.POST, label_suffix='')
     if form.is_valid():
         data = form.cleaned_data
         data['born'] = data['born'].strftime ('%F') # json don't handle datetime
@@ -33,5 +32,5 @@ def index (request):
         member_fp.write ('\n')
         member_fp.flush()
         messages.success (request, WELCOME_MSG)
-        form = MemberForm()
+#        form = MemberForm(label_suffix='')
     return dict (form=form)
