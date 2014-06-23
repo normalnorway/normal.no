a2f124dd072d8bfe6ab42d89d9b12c75917e4e67
Torkel Bjørnson-Langen <torkel@normal.no>
2014-06-23 18:15:15 +0200

    improved content blocks

--- django/apps/content/models.py
+++ django/apps/content/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+
+# @todo mark returned string as safe?
+def get_content (name):
+    ''' Return content block by name. If not found, substitute empty string'''
+    try:
+        obj = Content.objects.get (name=name)
+    except Content.DoesNotExist:
+        return ''   # @todo log
+    return obj.content
+
+
+# @todo fetch all args with one sql-query
+def get_content_dict (*args):
+    '''Return dict with name,content'''
+    ctx = dict()
+    for arg in args:
+        key = arg.replace ('-', '_')  # django template vars can't contain '-'
+        ctx[key] = get_content (arg)
+    return ctx
+
+
+
 class Content (models.Model):
     ''' Content block used by various views/templates '''
     name = models.CharField (max_length=64, unique=True)
--- django/apps/support/views.py
+++ django/apps/support/views.py
@@ -1,34 +1,32 @@
 # encoding: utf-8
-import os
-import json
-import datetime
-from django.shortcuts import redirect
-from django.contrib import messages
-from core.shortcuts import render_to
-from apps.content.models import Content
-from .forms import MemberForm
-
-from website.settings import BASE_DIR
-member_fp = open (os.path.join (BASE_DIR, 'db', 'newmembers'), 'a+')
-#import codecs
-#member_fp = codecs.open (os.path.join (BASE_DIR, 'db', 'newmembers'), 'a+', 'utf-8')
 
 WELCOME_MSG = u'''
 Velkommen som medlem og takk for støtten! Du vil i løpet av få dager mota
 en giro per e-post eller brev.
 '''
 
+import os
+import json
+import datetime
+from django.shortcuts import redirect
+from django.contrib import messages
+from core.shortcuts import render_to
+from apps.content.models import get_content_dict
+from .forms import MemberForm
+
+from website.settings import BASE_DIR
+member_fp = open (os.path.join (BASE_DIR, 'db', 'newmembers'), 'a+')
+
+#welcome_msg = get_content ('innmelding-ferdig') # @todo must be short!
+
 
 @render_to ('support:index.html')
 def index (request):
-    # sql in operator does not preserve order
-    #top, bottom = Content.objects.filter (name__in=['innmelding-top', 'innmelding-bunn'])
-    top     = Content.objects.get (name='innmelding-top')
-    bottom  = Content.objects.get (name='innmelding-bunn')
-
+    ctx = get_content_dict ('innmelding-top', 'innmelding-bunn')
     if not request.method == 'POST':
-        return dict (form=MemberForm(label_suffix=''), top=top.content, bottom=bottom.content)
-    form = MemberForm (request.POST, label_suffix='')
+        ctx['form'] = MemberForm()
+        return ctx
+    ctx['form'] = form = MemberForm (request.POST)
     if form.is_valid():
         data = form.cleaned_data
         data['born'] = data['born'].strftime ('%F') # json don't handle datetime
@@ -38,5 +36,4 @@ def index (request):
         member_fp.write ('\n')
         member_fp.flush()
         messages.success (request, WELCOME_MSG)
-#        form = MemberForm(label_suffix='')
-    return dict (form=form, top=top.content, bottom=bottom.content)
+    return ctx
