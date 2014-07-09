44e64f45257ac2710e277ad5036930a3fb3bc9e9
Torkel Bjørnson-Langen <torkel@normal.no>
2014-07-09 06:47:54 +0200

    cleanup news code

--- django/apps/news/forms.py
+++ django/apps/news/forms.py
@@ -1,26 +1,23 @@
 # -*- encoding: utf-8 -*-
-
 from django import forms
 
-# @todo own forms.Field
-#from core.forms import TextField
 
-# Note: required=True is default for form fields
+class SearchForm (forms.Form):
+    query = forms.CharField (max_length=100, label=u'Søk') # @todo html5 type=search?
 
-class NewsTips1 (forms.Form):
+
+
+# NOT IN USE
+
+class NewsTipsStep1 (forms.Form):
     url = forms.URLField (label='Adresse (url)')
     #url = forms.CharField (label='Lenke (URL)', max_length=64)
 
-class NewsTips2 (forms.Form):
+class NewsTipsStep2 (forms.Form):
     date = forms.DateField (label='Dato',
-                            #help_text=u'dd/mm/åå eller åååå-mm-dd',
+                            #help_text=u'dd/mm/åå eller åååå-mm-dd',    # @todo html5 placeholder?
                             input_formats = ('%Y-%m-%d', '%d/%m/%y', '%d. %B %Y'),
                             error_messages = {'invalid': u'Ugyldig dato. Bruk dd/mm/åå eller åååå-mm-dd'})
     #date = forms.DateField (label='Dato', help_text='Datoen artikkelen er publisert')
     title = forms.CharField (label='Overskrift', max_length=128)
     summary = forms.CharField (label='Ingress', widget=forms.Textarea)
-
-
-
-class SearchForm (forms.Form):
-    query = forms.CharField (max_length=100, label=u'Søk')
