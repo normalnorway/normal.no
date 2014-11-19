8a45cc88b49b258fdee43b08f9eb01567c23d52c
Torkel Bjørnson-Langen <torkel@normal.no>
2014-11-19 18:12:08 +0100

    Added TinyMCE editor for Content module

--- django/apps/content/admin.py
+++ django/apps/content/admin.py
@@ -1,4 +1,13 @@
+from django.db import models
 from django.contrib import admin
 from models import Content
+from tinymce4.widgets import TinyMCE
 
-admin.site.register (Content)
+# @todo use HtmlField in model instead
+
+class ContentAdmin (admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: { 'widget': TinyMCE },
+    }
+
+admin.site.register (Content, ContentAdmin)
