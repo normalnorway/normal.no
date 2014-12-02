27018965aae87a2b0e3062a3862e7d410ce4d5ec
Torkel Bjørnson-Langen <torkel@normal.no>
2014-12-02 21:17:10 +0100

    added edit-button for logged in users. added classes.less

--- django/apps/content/templatetags/content.py
+++ django/apps/content/templatetags/content.py
@@ -1,12 +1,21 @@
 from django import template
+from django.core.urlresolvers import reverse
 from apps.content.models import Content
 
 register = template.Library()
 
-@register.simple_tag
-def get_content (key):
+@register.simple_tag (takes_context=True)
+def get_content (context, key):
     try:
-        return Content.objects.get (name=key).content
+        obj = Content.objects.get (name=key)
     except Content.DoesNotExist:
         return '[ERROR: Unknown content block: %s]' % key
+
+    if not context['request'].user.is_staff:
+        return obj.content
+
+    #return EDIT_BUTTON_HTML % reverse ('admin:content_content_change', args=[obj.pk]) + obj.content
+    return obj.content + '<a href="%s" class="admin-edit-link">Rediger</a>' % \
+                reverse ('admin:content_content_change', args=[obj.pk])
+
     # Note: returned string is auto marked as safe
