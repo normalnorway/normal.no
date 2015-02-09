4232ca9652d7f7e8eff8eb3237cd8114a71ecea4
Torkel Bjørnson-Langen <torkel@normal.no>
2015-02-09 22:04:42 +0100

    bugfix and improvements in rss feed

--- django/apps/news/feed.py
+++ django/apps/news/feed.py
@@ -1,35 +1,62 @@
+# encoding: utf-8
 from datetime import datetime
 from django.contrib.syndication.views import Feed
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
 #from django.utils import timezone
 from . models import Article
 
 # @todo cache?
+# @todo enable ttl?
 
-# @todo description
-# I nyhetsarktivet finner du lenker til de fleste cannabis-relaterte saker
-# omtalt i norsk media. I tillegg til en del utenlandske saker.
+# Better description?
+#   I nyhetsarktivet finner du lenker til de fleste cannabis-relaterte saker
+#   omtalt i norsk media. I tillegg til en del utenlandske saker.
+#   Arkiv: http://normal.no/nyheter/arkiv/
 
 class NewsFeed (Feed):
     title = 'Normals nyhetsarkiv'
-    link = '/rss/'
     description = u'Cannabisrelaterte nyheter i norske medier. Arkiv: http://normal.no/nyheter/arkiv/'
-    author_name = 'Normal'
-    author_email = 'post@normal.no'
-    #ttl = 600
+    categories = ('cannabis', 'hasj', 'marihuana', 'rus', 'ruspolitikk', 'nyheter')
+    link = '/nyheter/rss/'
+
+    # These have no effect :(
+    #author_name = u'Normal – Norsk organisasjon for reform av marihuanalovgivningen'
+    #author_email = 'post@normal.no'
+    #author_link = 'http://normal.no/om'
+    #feed_copyright = 'No Copyright (c) 2015, Normal'
+    # But author_* is supported per article by using item_author_*()
+
+    # Not included if not set
+    #ttl = 3600
+
 
     def items (self):
+        """ Articles to include in the feed """
         return Article.objects.order_by ('-date')[:25]
+        #return Article.objects.exclude(url__isnull=True).order_by ('-date')[:25]
+
+
+    ## Handle item fields: item_<field>
 
     def item_link (self, item):
-        return item.url # link directly to external news story
+        return item.url if item.url else item.get_absolute_url()
+        #return item.url if item.url else reverse ('news-detail', args=[item.id])
+        # Articles without url  => link to own view
+        # Articles with url     => link directly to external news story (url)
 
-    # Note: will set Last-Modified header based on this
     def item_pubdate (self, item):
+        """ Note: will set Last-Modified header based on this """
         return datetime.combine (item.date, datetime.min.time())
         # @todo use real time, not fake it
 
     def item_description (self, item):
         return item.summary
 
-    # title -> title
+    # Note: Need this. Without the string is not marked as safe,
+    # and there will be troubles with quotes, etc.
+    def item_title (self, item):
+        return item.title
+
+    # @todo enable and return true for articles that have canonical url
+    #def item_guid_is_permalink (self, obj):
+    #    return obj.permalink != ''
