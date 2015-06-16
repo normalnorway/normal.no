ad1c501bcd01119a8d5890bd2a698f166f6c6fa9
Torkel Bjørnson-Langen <torkel@normal.no>
2015-06-17 01:50:19 +0200

    landscape.io for PR #7

--- django/apps/polls/admin.py
+++ django/apps/polls/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 
-from .models import *
+from .models import Question, Choice
 
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
-	
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
@@ -14,4 +14,3 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-# Register your models here.
--- django/apps/polls/views.py
+++ django/apps/polls/views.py
@@ -1,23 +1,21 @@
-from django.http import HttpResponse,  HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.shortcuts import get_object_or_404, render
 from django.core.urlresolvers import reverse
 
 from .models import Question,  Choice
-# Create your views here.
 
 def index(request):
-	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-	template = loader.get_template('polls/index.html')
-	context = RequestContext(request, {
-	'latest_question_list': latest_question_list,
-	})
-	return HttpResponse(template.render(context))
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
 	
 def detail(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
-	return render(request, 'polls/detail.html', {'question': question})
-	return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
