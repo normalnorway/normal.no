d3264605e916e59f87f9949c64e73886ab27840c
Torkel Bjørnson-Langen <torkel@normal.no>
2015-07-17 05:52:54 +0200

    fixed tests

--- django/tests.py
+++ django/tests.py
@@ -1,35 +1,67 @@
+# encoding: utf-8
+# $ ./manage.py test tests
 from django.test import TestCase, Client
-from django.core import urlresolvers
-from website import urls
 
-# TODO
-# nyheter/  (included urls)
-# own testcase per url (better to manually add?)
+# @todo insert test data
 
-class GlobalTestCase (TestCase):
-    def setUp (self):
-        self.client = Client()
+class TestUrls (TestCase):
+    def _http_status (self, url):
+        return self.client.get (url).status_code
+        #return self.client.head (url).status_code
 
-    def test_views (self):
-        self.assertTrue (True)  # tmp disabled
-        return
+    def test_home (self):
+        self.assertEqual (200, self._http_status ('/'))
+    def test_nyhetsbrev (self):
+        self.assertEqual (200, self._http_status ('/nyhetsbrev/'))
+    def test_bli_medlem (self):
+        self.assertEqual (200, self._http_status ('/bli-medlem/'))
+    def test_opprop (self):
+        self.assertEqual (200, self._http_status ('/opprop/'))
+    def test_nettguide (self):
+        self.assertEqual (200, self._http_status ('/nettguide/'))
+    def test_nyhetsbrev_1 (self):
+        self.assertEqual (200, self._http_status ('/nyhetsbrev/1/'))
+    # These are in cms.Pages
+#    def test_om_normal (self):
+#        self.assertEqual (200, self._http_status ('/om-normal/'))
+#    def test_on_normal_ledelsen (self):
+#        self.assertEqual (200, self._http_status ('/om-normal/ledelsen/'))
+#    def test_stott (self):
+#        self.assertEqual (200, self._http_status (u'/støtt/'))
+#    def test_om_cannabis (self):
+#        self.assertEqual (200, self._http_status ('/om-cannabis/'))
+#    def test_faq (self):
+#        self.assertEqual (200, self._http_status ('/faq/'))
+#    def test_medisin (self):
+#        self.assertEqual (200, self._http_status ('/medisin/'))
+#    def test_visjon (self):
+#        self.assertEqual (200, self._http_status ('/visjon/'))
+#    def test_europa (self):
+#        self.assertEqual (200, self._http_status ('/europa/'))
 
-        """Check that all views from website.urls returns http success"""
-        for obj in urls.urlpatterns:
-            if isinstance (obj, urlresolvers.RegexURLPattern):
-                if obj.name and 'password_reset' in obj.name: continue
-                assert obj.regex.pattern[-1] == '$'
-                # @todo don't just blindly chop of first and last char
-                #       replace first '^' with '/'
-                #       chop last if '$'
-                url = '/' + obj.regex.pattern[1:-1] # drop first and last char (regex anchors)
-                if url.startswith ('/edit/'): continue  # non-public views
-                res = self.client.head (url)
-                if obj.callback.__name__ == 'RedirectView':
-                    self.assertEqual (res.status_code, 301)
-                else:
-                    self.assertEqual (res.status_code, 200)
 
-            elif isinstance (obj, urlresolvers.RegexURLResolver):
-                # @todo recurse on o.url_patterns, but skip if o.namespace == 'admin'
-                pass
+
+class TestRedirects (TestCase):
+    '''Test all (permantent) redirect views/urls'''
+    def test_medlem (self):
+        res = self.client.head ('/medlem/')
+        self.assertEqual (res.status_code, 301)
+        self.assertTrue (res['location'].endswith ('/bli-medlem/'))
+    def test_rss (self):
+        res = self.client.head ('/rss/')
+        self.assertEqual (res.status_code, 301)
+        self.assertTrue (res['location'].endswith ('/nyheter/rss/'))
+    def test_gruppesoksmaal (self):
+        res = self.client.head (u'/gruppesøksmaal/')
+        self.assertEqual (res.status_code, 301)
+        self.assertTrue (res['location'].endswith ('/sider/gruppesoksmaal/'))
+    def test_frivillig (self):
+        res = self.client.head ('/frivillig/')
+        self.assertEqual (res.status_code, 301)
+        self.assertTrue (res['location'].endswith ('/sider/frivillig/'))
+    '''
+    def test_ (self):
+        res = self.client.head ('')
+        self.assertEqual (res.status_code, 301)
+        self.assertTrue (res['location'].endswith (''))
+    '''
