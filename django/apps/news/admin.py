b7114632a55e58ceb0877300452314b8fe7f58a8
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-10-17 02:18:23 +0200

    news.admin: new list action to set published=True
    and some code cleanup

--- django/apps/news/admin.py
+++ django/apps/news/admin.py
@@ -1,7 +1,9 @@
+import urlparse
 from django.contrib import admin
 from .models import Article
 
 
+# @todo can drop this. can achieve the same by sorting by has_body
 class LinkOnlyFilter (admin.SimpleListFilter):
     def lookups (self, request, model_admin):
         return (('1', 'Yes'), ('0', 'No'))
@@ -11,7 +13,7 @@ class LinkOnlyFilter (admin.SimpleListFilter):
         if v == '0': return queryset.filter (body='')
         if v == '1': return queryset.exclude (body='')
         assert False
-    title = 'Has body?'
+    title = 'Own comment'
     parameter_name = 'body'
 
 
@@ -21,15 +23,19 @@ class ArticleAdmin (admin.ModelAdmin):
     date_hierarchy = 'date'
     fields = ('date', ('url', 'url_is_canonical'), 'title', 'summary', 'body', 'image_url', 'published')
     search_fields = ('title', 'summary', 'body')
-    list_display = ('_date', 'title', 'domain', 'published', 'has_body')
+    actions = ('action_publish',)
+    list_display = ('_date', 'title', '_domain', 'published', 'has_body')
     list_display_links = ('_date', 'title',)
     list_filter = ('date', 'published', LinkOnlyFilter)
     list_per_page = 50  # default is 100
 
+    def action_publish (self, request, queryset):
+        queryset.update (published=True)
+    action_publish.short_description = 'Publiser valgte nyhets-lenker'
+
     def _date (self, obj):
         if obj.date: return obj.date.strftime ('%F')
-        # Q: for which objects are date missing?
-        #return obj.date.strftime ('%F')
+        #return obj.date.strftime ('%F') # Q: for which objects are date missing?
     _date.admin_order_field = 'date'
     _date.short_description = 'date'
 
@@ -38,23 +44,17 @@ class ArticleAdmin (admin.ModelAdmin):
     def has_body (self, obj):
         return obj.body != ''
     has_body.boolean = True
-    has_body.short_description = 'Has body'
-    #has_body.admin_order_field = 'body'
+    has_body.short_description = 'own comment'
+    has_body.admin_order_field = 'body'
 
-    # @todo urlparse instead of regex
-    import re
-    _re_domain = re.compile (r'^(http://)?(www\.)?([^/]+)', re.I)
-    def domain (self, obj):
-        #domain = self._re_domain.match (obj.url).group(2)
-        if not obj.url: return ''  # url can be NULL
-        match = self._re_domain.match (obj.url)
-        if not match: return ''
-        domain = match.group(3)
+    def _domain (self, obj):
+        urlobj = urlparse.urlsplit (obj.url)
+        s = urlobj.hostname
+        domain = s[4:] if s.startswith('www.') else s
         return '<a href="%s" title="%s" target="_blank">%s</a>' % (obj.url, obj.url, domain)
-    domain.allow_tags = True
-    domain.short_description = 'Lenke'
-    domain.admin_order_field = 'url'
-
+    _domain.allow_tags = True
+    _domain.short_description = 'Domene'
+    _domain.admin_order_field = 'url'
 
 
 admin.site.register (Article, ArticleAdmin)
