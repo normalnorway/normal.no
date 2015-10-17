9b7766114db17f641cfd83ea8dfaeeda8786b9db
Torkel Bjørnson-Langen <torkel@innovatio.no>
2015-10-17 21:44:45 +0200

    news: clean up admin.py

--- django/apps/news/admin.py
+++ django/apps/news/admin.py
@@ -3,30 +3,16 @@ from django.contrib import admin
 from .models import Article
 
 
-# @todo can drop this. can achieve the same by sorting by has_body
-class LinkOnlyFilter (admin.SimpleListFilter):
-    def lookups (self, request, model_admin):
-        return (('1', 'Yes'), ('0', 'No'))
-    def queryset (self, request, queryset):
-        v = self.value()
-        if v is None: return queryset
-        if v == '0': return queryset.filter (body='')
-        if v == '1': return queryset.exclude (body='')
-        assert False
-    title = 'Own comment'
-    parameter_name = 'body'
-
-
-
+@admin.register (Article)
 class ArticleAdmin (admin.ModelAdmin):
     ordering = ('-date',)
     date_hierarchy = 'date'
     fields = ('date', ('url', 'url_is_canonical'), 'title', 'summary', 'body', 'image_url', 'published')
-    search_fields = ('title', 'summary', 'body')
+    search_fields = ('title', 'summary', 'body', 'url')
     actions = ('action_publish',)
-    list_display = ('_date', 'title', '_domain', 'published', 'has_body')
+    list_display = ('_date', 'title', '_domain', 'published', 'has_summary', 'has_body')
     list_display_links = ('_date', 'title',)
-    list_filter = ('date', 'published', LinkOnlyFilter)
+    list_filter = ('date', 'published')
     list_per_page = 50  # default is 100
 
     def action_publish (self, request, queryset):
@@ -35,11 +21,19 @@ class ArticleAdmin (admin.ModelAdmin):
 
     def _date (self, obj):
         if obj.date: return obj.date.strftime ('%F')
-        #return obj.date.strftime ('%F') # Q: for which objects are date missing?
+        #return obj.date.strftime ('%F')
+        # Q: for which objects are date missing? A: auto-added, with unparsable data
     _date.admin_order_field = 'date'
     _date.short_description = 'date'
 
-    # http://stackoverflow.com/questions/4067712/django-admin-adding-pagination-links-in-list-of-objects-to-top
+    def _domain (self, obj):
+        urlobj = urlparse.urlsplit (obj.url)
+        s = urlobj.hostname
+        domain = s[4:] if s.startswith('www.') else s # bug: messes up sorting
+        return '<a href="%s" title="%s" target="_blank">%s</a>' % (obj.url, obj.url, domain)
+    _domain.allow_tags = True
+    _domain.short_description = 'Domene'
+    _domain.admin_order_field = 'url'
 
     def has_body (self, obj):
         return obj.body != ''
@@ -47,14 +41,7 @@ class ArticleAdmin (admin.ModelAdmin):
     has_body.short_description = 'own comment'
     has_body.admin_order_field = 'body'
 
-    def _domain (self, obj):
-        urlobj = urlparse.urlsplit (obj.url)
-        s = urlobj.hostname
-        domain = s[4:] if s.startswith('www.') else s
-        return '<a href="%s" title="%s" target="_blank">%s</a>' % (obj.url, obj.url, domain)
-    _domain.allow_tags = True
-    _domain.short_description = 'Domene'
-    _domain.admin_order_field = 'url'
-
-
-admin.site.register (Article, ArticleAdmin)
+    def has_summary (self, obj):
+        return bool(obj.summary)
+    has_summary.boolean = True
+    has_summary.admin_order_field = 'summary'
